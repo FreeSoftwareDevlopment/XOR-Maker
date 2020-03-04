ea1b29b0b01951c63c4094d92a0951dd06c514f3
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-03-04 21:09:34 +0100

    Update minify.js

--- minify.js
+++ minify.js
@@ -1,13 +1,14 @@
 const fs=require("fs");
-const currentcodename="anne";
+const currentcodename="sharkMin";
 const path=__dirname+"/dist";
-console.log("UTF-8 Minify\n\tCodename: " + currentcodename);
+console.log("UTF-8 Minify\n\tCodename: " + currentcodename+"\n");
 fs.readdir(path, function (err, files) {
 	if(err){
 		console.warn("ERROR IN DIRLIST");
 	}
 	files.forEach(function (file) {
         fs.readFile(path+"/"+file, {encoding: 'utf-8'}, function(err,data){
+		console.log("Begin: "+file);
         	const towrire=data.split("\n").join("");
         	fs.writeFile(__dirname+"/bin/"+file,towrire,(errss)=>{
         		if(!errss){
